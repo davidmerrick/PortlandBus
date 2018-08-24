b48dd2e040345c02883048cbc2ada48d257f13e2
David Merrick <david@david-merrick.com>
2018-08-24 01:39:33 -0700

    Added passing test for invalid input.

--- src/test/java/com/merricklabs/portlandbus/PortlandBusIntegrationTestBase.java
+++ src/test/java/com/merricklabs/portlandbus/PortlandBusIntegrationTestBase.java
@@ -2,6 +2,8 @@ package com.merricklabs.portlandbus;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.merricklabs.portlandbus.mocks.MockTrimetClient;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 import static com.merricklabs.portlandbus.testutil.TestConstants.INTEGRATION_GROUP;
@@ -15,4 +17,13 @@ public class PortlandBusIntegrationTestBase {
         injector = Guice.createInjector(new PortlandBusTestModule());
     }
 
+    @AfterMethod(groups = INTEGRATION_GROUP)
+    public void afterMethod() {
+        resetMocks();
+    }
+
+    private void resetMocks() {
+        MockTrimetClient mockTrimetClient = injector.getInstance(MockTrimetClient.class);
+        mockTrimetClient.reset();
+    }
 }
